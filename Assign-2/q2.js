1bcbba8b63a9ad2febb7b6dfd0716d85a1036060
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-11 15:41:20 +0500

    q2.js

---
+++ Assign-2/q2.js
@@ -0,0 +1,2 @@
+let fullName = "Syed Munir Ullah"; //Person Name
+console.log(`“Hello ${fullName}, would you like to learn some Python today?”`);        //“Hello Syed Munir Ullah, would you like to learn some Python today?”
