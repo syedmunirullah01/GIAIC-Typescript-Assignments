beb89e63554ea36c2163db0f6aceff98faeb702c
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-11 17:59:07 +0500

    Delete Assign-5 directory

--- Assign-5/q5.ts
+++
@@ -1,9 +0,0 @@
-let  famous_person = "Mark ZuckerBerg";
-let famous_Quote = "The biggest risk is not taking any risk in a world that is changing really quickly, the only strategy that is guaranteed to fail is not taking risks.";
-let message = `${famous_person} once said, ${famous_Quote}`
-console.log(message);
-
-//Mark ZuckerBerg once said, The biggest risk is not taking any risk in a world that is changing really quickly, the only strategy that is guaranteed to fail is not taking risks.
-
-
-
