a6a85b9fd46d3c5301692ab54332d5d95ed09611
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-12 00:13:17 +0500

    Create q10.js

---
+++ Assign-10/q10.js
@@ -0,0 +1,12 @@
+// Store a persons name in a variable and print a message to that person
+
+let fullName = "Syed Munir Ullah";
+console.log(`Hello ${fullName}, would you like to learn some programming Today?`);
+
+// Famous Quote by Mark Zuckerberg
+const author = "Mark Zuckerberg";
+const quote = "The biggest risk is not taking any risk in a world that is changing really quickly, the only strategy that is guaranteed to fail is not taking risks.";
+console.log(`${author} once said, ${quote}`);
+
+
+`This tasks shows us how we can print a message who wrote that message`;
