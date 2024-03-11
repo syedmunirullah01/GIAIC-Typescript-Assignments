eec9c2a85417fd8b88b3884f47b5d63239e9ddb8
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-11 17:28:48 +0500

    q3.ts

---
+++ Assign-3/q3.ts
@@ -0,0 +1,11 @@
+let personName = "Syed Munir Ullah";
+
+// Uppercase in typescript
+console.log(personName.toUpperCase());  //SYED MUNIR ULLAH
+
+// Lowercase in typescript
+console.log(personName.toLowerCase());  //syed munir ullah
+
+// Titlecase in typescript
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1))    //Syed Munir Ullah
+
