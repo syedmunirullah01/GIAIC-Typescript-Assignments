cb55d4f6182053f6f9c9252ccb3b2215b695337c
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-11 17:44:16 +0500

    q4.ts

---
+++ Assign-4/q4.ts
@@ -0,0 +1,6 @@
+const famousQuote = "The biggest risk is not taking any risk in a world that is changing really quickly, the only strategy that is guaranteed to fail is not taking risks.";
+const authorName = "Mark ZuckerBerg"
+
+console.log(`${authorName} once said, ${famousQuote}`);
+
+// Mark Zuckerberg once said, The biggest risk is not taking any risk in a world that is changing really quickly, the only strategy that is guaranteed to fail is not taking risks.
