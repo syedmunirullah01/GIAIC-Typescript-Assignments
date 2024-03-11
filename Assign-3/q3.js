f992fa2dc5fa1ed6d5027f1f996b5ab994ab6fed
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-11 17:30:48 +0500

    q3.js

---
+++ Assign-3/q3.js
@@ -0,0 +1,7 @@
+let personName = "Syed Munir Ullah";
+
+console.log(personName.toUpperCase()); //SYED MUNIR ULLAH
+
+console.log(personName.toLowerCase());  //syed munir ullah
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1));     //Syed Munir Ullah
