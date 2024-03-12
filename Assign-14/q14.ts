00b1d9ab6c09e488ca59a30fd23930d18a33b273
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-12 22:12:40 +0500

    Create q14.ts

---
+++ Assign-14/q14.ts
@@ -0,0 +1,6 @@
+let invitedGuests : string[] =[ "Mark Zukerberg", "Albert Einstein", "Elon Musk"]
+
+invitedGuests.forEach (invitedGuests => {
+    console.log(`Dear ${invitedGuests} , Would You Like To Join Me at Dinner?`);
+    
+})
