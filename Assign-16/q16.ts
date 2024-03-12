8f17a98f6b57150bd315c3d2e463f4b8f535f754
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-12 22:48:33 +0500

    Create q16.ts

---
+++ Assign-16/q16.ts
@@ -0,0 +1,13 @@
+
+let invitedGuests : string[] =[ "Mark Zukerberg", "Albert Einstein", "Elon Musk"]
+
+console.log("Great news! I found a bigger dinner table!");
+
+//Adding more guests
+invitedGuests.unshift("Issac Newton");
+invitedGuests.splice(invitedGuests.length / 2,0, "Charles Darwin");
+
+invitedGuests.forEach(invitedGuests => {
+    console.log(`Dear ${invitedGuests}, would you like to join me for dinner?`);
+    
+});
