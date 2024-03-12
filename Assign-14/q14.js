6aeae1daa622a17f030e796a3cc3ee36970512c7
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-12 22:13:03 +0500

    Create q14.js

---
+++ Assign-14/q14.js
@@ -0,0 +1,5 @@
+let invitedGuests = ["Mark Zukerberg", "Albert Einstein", "Elon Musk"];
+
+invitedGuests.forEach(invitedGuests => {
+    console.log(`Dear ${invitedGuests} , Would You Like To Join Me at Dinner?`);
+});
