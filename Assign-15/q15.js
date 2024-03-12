b3cd18b3f7abf89d6c7aaf90e789bd8076cce172
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-12 22:36:50 +0500

    Update q15.js

--- Assign-15/q15.js
+++ Assign-15/q15.js
@@ -1,11 +1,15 @@
-let guests = ["Mark Zukerberg", "Nikola Tesla", "Marie Curie"];
+let invitedGuests = ["Mark Zukerberg", "Albert Einstein", "Elon Musk"];
+
+invitedGuests.forEach(invitedGuests => {
+    console.log(`Dear ${invitedGuests} , Would You Like To Join Me at Dinner?`);
+});
 
 let notToAttend = "Mark Zukerberg";
 console.log(`${notToAttend} can't make it to dinner.`);
 
-let newGuest = "Elon Musk";
-guests[guests.indexOf(notToAttend)] = newGuest;
+let newGuest = "Nikola Tesla";
+invitedGuests[invitedGuests.indexOf(notToAttend)] = newGuest;
 
-guests.forEach(guest => {
+invitedGuests.forEach(guest => {
     console.log(`Dear ${guest}, would you like to join me for dinner?`);
 });
