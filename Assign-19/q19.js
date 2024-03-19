c50ada121ffb402bc3c4ebb21714f5851101e124
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-19 16:16:05 +0500

    Create q19.js

---
+++ Assign-19/q19.js
@@ -0,0 +1,30 @@
+var callFrens = ["Awais", "Shahnoor", "Samad"];
+var notInTheCity = "Shahnoor";
+function callFriends() {
+    callFrens.forEach(function (name, i) {
+        if (name === notInTheCity) {
+            callFrens[i] = "Ayan";
+            console.log("Hi ".concat(name, " i would like to dinner with you thats why i'm inviting you"));
+        }
+        else {
+            console.log("Hi ".concat(name, " i would like to dinner with you thats why i'm inviting you"));
+        }
+    });
+}
+callFriends();
+console.log("Hello guy's i would like to do a bigger dinner with you");
+callFrens.unshift("Ahmed");
+callFrens.splice(2, 0, "Murtaza");
+callFrens.push("Ibrahim");
+callFriends();
+console.log("I will invite only two frens");
+var array_length = callFrens.length;
+for (var i = array_length; i > 2; i--) {
+    console.log("Sorry ".concat(callFrens[i - 1], " you are not invited because of space issue"));
+    callFrens.pop();
+}
+console.log(callFrens);
+console.log(callFrens.length);
+callFriends();
+callFrens.length = 0;
+console.log(callFrens);
