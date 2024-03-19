34bcb18d1fb794c082cb1e8d8db205736c0d5de3
Syed Munir Ullah <145977877+syedmunirullah01@users.noreply.github.com>
2024-03-19 16:09:50 +0500

    Create q18.ts

---
+++ Assign-18/q18.ts
@@ -0,0 +1,19 @@
+let places = ["India", "Turkey", "Usa", "Nigeria", "Saudia"];
+console.log(places);
+
+let convertPlaces = places.slice().sort()
+console.log(convertPlaces);
+console.log(places);
+
+let convertPlacesReverse = convertPlaces.reverse()
+console.log(convertPlacesReverse);
+console.log(places);
+
+places.reverse();
+console.log(places);
+places.reverse()
+console.log(places);
+
+places.sort()
+console.log(places);
+console.log(places.reverse());
